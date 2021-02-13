235d549d728ca86116b2001c14671e2dd5ddafe0
Leif Bersamina <leifbersamina@gmail.com>
2021-02-13 21:21:47 +0800

    Updated Landing Page

---
+++ Landing_Page/script.js
@@ -0,0 +1,62 @@
+const burgerMenu = document.querySelector(".burger");
+const menu = document.querySelector(".menu");
+
+const menuLinks = document.querySelectorAll(".menu li a");
+
+// console.log(menuLinks);
+
+function openMenu(){
+    menu.classList.toggle("menu-screen");
+}
+
+function transition(){
+    menu.classList.toggle("transfer");
+}
+
+burgerMenu.addEventListener("click",openMenu);
+
+menu.addEventListener("click",openMenu);
+
+
+//Slider Embedden Video
+
+const videoContainer = document.querySelector('#video');
+const videos = document.querySelectorAll('#video iframe');
+
+const prevBtn = document.querySelector('#prev');
+const nextBtn = document.querySelector('#next');
+
+let counter = 1;
+
+const vidSize = videos[0].clientWidth;
+
+videoContainer.style.transform = 'translateX(' + (-vidSize * counter) + 'px)';
+
+nextBtn.addEventListener('click', function(){
+    if(videos[counter].id === 'last-vid'){
+        videos.style.transform = 'none';
+    }
+    else{
+        videoContainer.style.transition = 'transform 0.4s ease-in-out';
+        counter++;
+        console.log(counter);
+        videoContainer.style.transform = 'translateX(' + (-vidSize * counter) + 'px)';
+    }
+});
+
+prevBtn.addEventListener('click', function(){
+    if(videos[counter].id === 'first-vid'){
+        videos.style.transform = 'none';
+    }
+    else{
+        videoContainer.style.transition = 'transform 0.4s ease-in-out';
+        counter--;
+        console.log(counter);
+        videoContainer.style.transform = 'translateX(' + (-vidSize * counter) + 'px)';
+    }
+    
+});
+
+
+
+
