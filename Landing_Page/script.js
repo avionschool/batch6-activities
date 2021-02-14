701fb48abd9e5fcb648ec340df6bbb284006fb05
Leif Bersamina <leifbersamina@gmail.com>
2021-02-15 00:25:34 +0800

    Updated Landing Page

--- Landing_Page/script.js
+++ Landing_Page/script.js
@@ -1,6 +1,5 @@
 const burgerMenu = document.querySelector(".burger");
 const menu = document.querySelector(".menu");
-
 const menuLinks = document.querySelectorAll(".menu li a");
 
 // console.log(menuLinks);
@@ -14,7 +13,6 @@ function transition(){
 }
 
 burgerMenu.addEventListener("click",openMenu);
-
 menu.addEventListener("click",openMenu);
 
 
@@ -22,7 +20,6 @@ menu.addEventListener("click",openMenu);
 
 const videoContainer = document.querySelector('#video');
 const videos = document.querySelectorAll('#video iframe');
-
 const prevBtn = document.querySelector('#prev');
 const nextBtn = document.querySelector('#next');
 
@@ -39,7 +36,7 @@ nextBtn.addEventListener('click', function(){
     else{
         videoContainer.style.transition = 'transform 0.4s ease-in-out';
         counter++;
-        console.log(counter);
+        //console.log(counter);
         videoContainer.style.transform = 'translateX(' + (-vidSize * counter) + 'px)';
     }
 });
@@ -59,4 +56,18 @@ prevBtn.addEventListener('click', function(){
 
 
 
+//For Header Animation
+const navBar = document.querySelector('nav');
+console.log(navBar);
 
+function scrollDown(){
+    const downScroll = window.pageYOffset;
+    if(downScroll >5){
+        navBar.classList.add('scroll');
+    }
+    else{
+        navBar.classList.remove('scroll');
+    }
+}
+
+window.addEventListener('scroll', scrollDown);
