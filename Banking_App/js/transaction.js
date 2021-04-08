9fcb54f37fd4f9f24c1abf1aaad686ea5e7cd2d6
Leif Bersamina <leifbersamina.gmail.com>
2021-04-08 21:39:09 +0800

    Create Banking App

---
+++ Banking_App/js/transaction.js
@@ -0,0 +1,60 @@
+
+//=============== DEPOSIT =======================//
+const depositor = {
+    name: document.querySelector('#deposit #deposit-form #depositor'),
+    balance: document.querySelector('#deposit #deposit-form #dep-amnt'),
+}
+const depositMoney = document.querySelector('#deposit #deposit-form #deposit-btn');
+
+depositMoney.addEventListener('click', addMoney);
+function addMoney(e){
+    if(depositor.name.value.length > 1 && depositor.balance.value > 0){
+        e.preventDefault();
+        if(bankApp.currentUsers.some(item => item.name ===depositor.name.value.toUpperCase())){
+            // deposit(depositor.name.value, depositor.balance.value);
+            valideDeposit(depositor.name.value, depositor.balance.value)
+            location.reload();
+        }
+
+        else if(!bankApp.currentUsers.some(item => item.name === depositor.name.value)){
+            alert('User does not Exist!!');
+        }
+    }
+}
+
+//=================== WITHDRAW ===========================//
+
+const withdrawer = {
+    name: document.querySelector('#withdraw #withdraw-form #withdrawer'),
+    balance: document.querySelector('#withdraw #withdraw-form #with-amnt'),
+}
+const withdrawMoney = document.querySelector('#withdraw #withdraw-form #withdraw-btn');
+
+withdrawMoney.addEventListener('click', removeMoney);
+function removeMoney(e){
+    if(withdrawer.name.value.length > 1 && withdrawer.balance.value > 0){
+        e.preventDefault();
+        if(bankApp.currentUsers.some(item => item.name === withdrawer.name.value.toUpperCase())){
+            // withdraw(withdrawer.name.value, withdrawer.balance.value);
+            validateWithdraw(withdrawer.name.value, withdrawer.balance.value);
+            location.reload();
+        }
+        else{
+            alert('User does not Exist!!');
+        }
+    }
+}
+
+// function calcWithdraw(){
+//     for(i =0; i <bankApp.currentUsers.length; i++){
+//         if(bankApp.currentUsers[i].name === withdrawer.name.value && bankApp.currentUsers[i].balance >= withdrawer.balance.value){
+//             bankApp.currentUsers[i].balance -= parseInt(withdrawer.balance.value);
+//             let newVal = JSON.stringify( bankApp.currentUsers[i]);
+//             localStorage.setItem(`${i}`, newVal);
+//             alert('Successful Transaction!!');
+//         }
+//         else if(bankApp.currentUsers[i].name === withdrawer.name.value && bankApp.currentUsers[i].balance < withdrawer.balance.value){
+//             return alert('Insufficient Fund!!');
+//         }
+//     }
+// }
