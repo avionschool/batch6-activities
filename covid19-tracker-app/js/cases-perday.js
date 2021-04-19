08f1159681855923fc4858faef2ccd509f2ad15a
Leif Bersamina <leifbersamina.gmail.com>
2021-04-19 23:47:18 +0800

    update covid-19-tracker

---
+++ covid19-tracker-app/js/cases-perday.js
@@ -0,0 +1,177 @@
+
+//================================== HTML ELEMENTS ==============================================================//
+const searchQuery = document.querySelector('#tracker-page #search-query');
+const submitDateFrom = document.querySelector('#tracker-page #date-from');
+const submitDateTo = document.querySelector('#tracker-page #date-to');
+const submitQuery = document.querySelector('#tracker-page #submit-btn');
+
+submitQuery.addEventListener('click', function(e){
+    if(searchQuery.value.length > 1 && submitDateFrom.value.length > 7 && submitDateTo.value.length > 7 ){
+        e.preventDefault()
+        displayCases(searchQuery.value,submitDateFrom.value, submitDateTo.value)
+    }
+})
+
+//=========================== LIST OF COUNTRIES ================================//
+let listCountries = [];
+const date = [];
+const cases = {
+    confirmed :[],
+    deaths : [],
+    recovered: []
+}
+async function displayCases(country,from, to){
+    let data = await fetch('https://api.covid19api.com/countries');
+    let json = await data.json();
+    let arrayCountries = await pushArray(listCountries, json);
+    let Slug = await getSlug(arrayCountries, country);
+    
+    let confirmedData = await fetch(`https://api.covid19api.com/total/country/${Slug}/status/confirmed?from=${from}T00:00:00Z&to=${to}T00:00:00Z`);
+    let confirmedjson = await confirmedData.json();
+
+    let deathsData = await fetch(`https://api.covid19api.com/total/country/${Slug}/status/deaths?from=${from}T00:00:00Z&to=${to}T00:00:00Z`);
+    let deathsjson = await deathsData.json();
+
+    let recoveredData = await fetch(`https://api.covid19api.com/total/country/${Slug}/status/recovered?from=${from}T00:00:00Z&to=${to}T00:00:00Z`);
+    let recoveredjson = await recoveredData.json();
+    console.log(recoveredjson);
+    let objJson = await pushJSON(confirmedjson, deathsjson, recoveredjson)
+    console.log(objJson)
+    let table = await inserTable(cases.confirmed, cases.deaths, cases.recovered, arrayCountries, country)
+    return table;
+}
+
+function pushArray(arr, obj){
+    return new Promise(function(resolve){
+        for(key in obj){
+            arr.push(obj[key]);
+        }
+        resolve(arr);
+    })
+}
+
+function pushJSON(confirmed, deaths, recovered){
+    return new  Promise(function(resolve){
+            cases.confirmed.length = 0;
+            cases.deaths.length = 0;
+            cases.recovered.length = 0;
+            date.length = 0;
+
+        for(i=0; i < confirmed.length; i++){
+            cases.confirmed.push(confirmed[i].Cases)
+            cases.deaths.push(deaths[i].Cases)
+            cases.recovered.push(recovered[i].Cases)
+            date.push(confirmed[i].Date.slice(0,10))
+        }
+        resolve ({confirmed, deaths, recovered})
+    })
+}
+function getSlug(arr, value){
+    return new Promise(function(resolve){
+        let slug;
+        if(arr.some(item => item.Country === value)){
+            for(i=0; i < arr.length; i++){
+                if(arr[i].Country === value){
+                   slug = arr[i].Slug;
+                    
+                }
+            }
+        }
+        else{
+            alert('Country not found. Check the spelling of correct. Remember to CAPITALIZE the first beginning of the country name (e.g Japan, South Africa, United States of America')
+            slug = 'philippines';
+        }
+       
+        resolve (slug);
+    })
+}
+const tableContainer = document.querySelector('#tracker-page #table-container');
+function inserTable(confirmed, deaths, recovered, arr, country){
+    return new Promise(function(resolve){
+
+        createChart(date, cases.confirmed, 'confirmed','rgba(255, 166, 0, 0.339)');
+        createChart(date, cases.deaths, 'deaths', 'rgba(255, 0, 0, 0.339)');
+        createChart(date, cases.recovered, 'recovered', 'rgba(0, 255, 0, 0.339)');
+
+        tableContainer.innerHTML = '';
+        let table = document.createElement('table');
+        table.id = 'case-history'
+        table.setAttribute('cellspacing', '0');
+        tableContainer.appendChild(table);
+        let headRow = table.insertRow(0);
+        let headCell = [];
+        for(a=0; a < arr.length; a++){
+            if(arr[a].Country === country){
+                createTable(arr[a].ISO2);
+            }
+        }
+        
+        function createTable(arrCode){
+            if(headCell.length < 1){
+                for(j=0; j < 6; j++){
+                    headCell[j] = headRow.insertCell(-1);
+                    headCell[j].className = 'head-cell';
+                }
+                headCell[0].innerHTML = '<th>Flag</th>';
+                headCell[1].innerHTML = '<th>Country</th>';
+                headCell[2].innerHTML = '<th>Confirmed</th>';
+                headCell[3].innerHTML = '<th>Deaths</th>';
+                headCell[4].innerHTML = '<th>Recovered</th>';
+                headCell[5].innerHTML = '<th>Date</th>';
+            }
+            
+            for(i=0; i < confirmed.length; i++){
+                let row = table.insertRow(1);
+                let cell = [];
+                let img = document.createElement('img');
+                img.src = `https://www.countryflags.io/${arrCode}/flat/64.png`;
+                for(z = 0; z < 6; z++){
+                    cell[z] = row.insertCell(-1);
+                }
+                cell[0].append(img);
+                cell[1].append(country)
+                cell[2].append(confirmed[i]);
+                cell[3].append(deaths[i]);
+                cell[4].append(recovered[i]);
+                cell[5].append(date[i]);               
+            }
+        }
+        resolve(tableContainer);
+    })
+}
+
+//======================= GRAPH DATA ============================================//
+
+function createChart(arrDate, objData, status, bgColor){
+    let ctx = document.getElementById(`${status}-chart`).getContext('2d');
+    let myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: arrDate,
+            datasets: [{
+                label: status,
+                data: objData,
+                backgroundColor: [
+                    bgColor
+                ],
+                borderColor: [
+                    bgColor,
+                ],
+                borderWidth: 1,
+                fill: {
+                    target: 'origin',
+                    above: bgColor,   // Area will be red above the origin
+                }
+            }]
+        },
+        options: {
+            responsive: true,
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+}
+
--- covid19-tracker-app/js/main.js
+++ covid19-tracker-app/js/main.js
@@ -1,44 +1,49 @@
 
-//===================== FOR THE DROPDOWN SELECT TAG =======================//
-const optionSelector = document.querySelector('#countries');
-let selectedOpt = false;
-optionSelector.addEventListener('click', function(e){
-    let option = e.target;
-    if(option.value != '' && selectedOpt){
-      countrySelector(option.value)
-      return selectedOpt = false;
-    }
-    selectedOpt = true;
-})
 
-async function countrySelector(item){
-  let response = await fetch(`https://covid-api.mmediagroup.fr/v1/cases?country=${item}`);
-  let json = await response.json();
-  return json;
-}
-
+const topCases = []; // <= CONTAINS ALL DATA (CASES) OF EACH COUNTRY //
+iterateCountries();
 async function iterateCountries(){
   let countries = await fetch('https://covid-api.mmediagroup.fr/v1/cases?');
   let json = await countries.json();
-  for(let key in json){
-    let opt = document.createElement('option');
-    let createText = document.createTextNode(key);
-    opt.appendChild(createText);
-    opt.value = key;
-    optionSelector.appendChild(opt);
-  }
+  console.log(json)
+  let iterateCountyList = await iterateCountryList(json, topCases);
+  console.log(iterateCountyList);
 }
-iterateCountries();
 
-const topCases = [];
-//========================= DISPLAY WORLD CASES ==================================//
+//========================= DISPLAY GLOBAL CASE ==================================//
 iterateActiveCases();
-function displaytoWorld(arr){
+async function iterateActiveCases(){
+  let countries = await fetch('https://api.covid19api.com/summary');
+  let json = await countries.json();
+  console.log(json)
+  let push = await pushArray(topCases,json);
+  let displayWord = await displaytoWorld(json);
+  let topFive = await displayTopFive(push);
+}
+function pushArray(arr, data){
+  return new Promise(function (resolve, reject){
+    for(let key in data ){
+      if(key === 'Countries'){
+        for(let subKey in data[key]){
+          let items = data[key][subKey]
+          arr.push(items)
+        }
+      }
+    }
+    arr.sort((a,b)=>{
+      return b.TotalConfirmed - a.TotalConfirmed;
+    })
+    resolve (arr);
+  })
+}
+function displaytoWorld(obj){
   return new Promise(function(resolve, reject){
     const worldCase = document.querySelector('#world-cases');
     let heading = [];
     let subHeading = [];
-    for(i=0; i < 3; i++ ){
+    console.log(obj.Global)
+    let global = obj.Global;
+    for(i=0; i < 3;i++){
       let div = document.createElement('div');
       heading[i] = document.createElement('h2');
       subHeading[i] = document.createElement('h2');
@@ -50,47 +55,24 @@ function displaytoWorld(arr){
     heading[0].innerHTML = `TOTAL CONFIRMED `;
     heading[1].innerHTML = `TOTAL DEATHS`;
     heading[2].innerHTML = `TOTAL RECOVERIES`;
-    subHeading[0].innerHTML = arr[0].All.confirmed;
-    subHeading[1].innerHTML = arr[0].All.deaths;
-    subHeading[2].innerHTML = arr[0].All.recovered;
+    subHeading[0].innerHTML = global.TotalConfirmed;
+    subHeading[1].innerHTML = global.TotalDeaths;
+    subHeading[2].innerHTML = global.TotalRecovered;
     resolve (worldCase)
   })
 }
 
 //========================= FOR TOP 5 WITH ACTIVE CASES ==========================//
-async function iterateActiveCases(){
-  // let countriesOne = await fetch('https://api.covid19api.com/live/country/philippines');
-  // let json1 = await countriesOne.json();
-  // console.log(json1)
-
-  let countries = await fetch('https://covid-api.mmediagroup.fr/v1/cases?');
-  let json = await countries.json();
-  let push = await pushArray(topCases,json);
-  let topFive = await displayTopFive(push);
-  await displaytoWorld(push);
-}
-function pushArray(arr, data){
-  return new Promise(function (resolve, reject){
-    for(let key in data ){
-      arr.push(data[key]);
-    }
-    arr.sort((a,b)=>{
-      return b.All.confirmed - a.All.confirmed;
-    })
-    console.log(arr)
-    resolve (arr);
-  })
-}
 
 function displayTopFive(arr){
   return new Promise(function(resolve, reject){
     const topCountry = document.querySelector('#top-positive');
     let date = document.querySelector('#top-positive h1 span');
-    date.append(arr[7].All.updated)
-    for(i=1;i < 7; i++){
+    date.append(arr[7].Date.slice(0,10))
+    for(i=0;i < 6; i++){
       let div = document.createElement('div');
       let img = document.createElement('img');
-      img.src = `https://www.countryflags.io/${arr[i].All.abbreviation}/flat/64.png`;
+      img.src = `https://www.countryflags.io/${arr[i].CountryCode}/flat/64.png`;
       let h1 = document.createElement('h1');
       div.className = 'top-country-with-pos';
       let countryList = document.createElement('table');
@@ -98,16 +80,21 @@ function displayTopFive(arr){
       let row2 = countryList.insertRow(-1);
       let row3 = countryList.insertRow(-1);
       
-      h1.append(arr[i].All.country)
+      h1.append(arr[i].Country)
 
       row1.insertCell(0).innerHTML = '<strong>Confirmed Cases:</strong>';
-      row1.insertCell(1).append(arr[i].All.confirmed);
+      row1.insertCell(1).append(arr[i].TotalConfirmed);
 
       row2.insertCell(0).innerHTML = '<strong>Deaths:</strong>';
-      row2.insertCell(1).append(arr[i].All.deaths)
-      
-      row3.insertCell(0).innerHTML = '<strong>Population:</strong>';
-      row3.insertCell(1).append(arr[i].All.population);
+      row2.insertCell(1).append(arr[i].TotalDeaths)
+      row3.insertCell(0).innerHTML = '<strong>Recoveries:</strong>';
+      if(arr[i].TotalRecovered > 0){
+        row3.insertCell(1).append(arr[i].TotalRecovered);
+      }
+      else if(arr[i].TotalRecovered <= 0){
+        row3.insertCell(1).append('(Data not Available)');
+      }
+     
       
       topCountry.appendChild(div);
       div.appendChild(countryList);
@@ -121,40 +108,51 @@ function displayTopFive(arr){
 const displayCountries = document.querySelector('#list-countries button');
 const nation = document.querySelector('#list-countries');
 const nationChild = document.querySelector('#list-countries #country-container');
-const pageButtons = document.querySelector('#list-countries #paginator')
-displayCountries.addEventListener('click', displayAll)
+const pageButtons = document.querySelector('#list-countries #page-btns')
+let listClicked = false;
 
+displayCountries.addEventListener('click', function(){
+  if(listClicked === false){
+    displayAll();
+    displayCountries.innerText = 'Click to hide countries';
+    return listClicked = true;
+  }
+  else if(listClicked === true){
+    // listClicked = false;
+    displayCountries.innerText = 'Click to view more list of countries';
+    pageButtons.innerHTML ='';
+    listClicked = false;
+    while(nationChild.childNodes){
+    nationChild.removeChild(nationChild.childNodes[0]);
+    }
+    
+  }
+})
 
+let counter =0;
 let currentPage = 1;
 let countryBlock = 6;
 async function displayAll(){
   let iterate = await getCountry(topCases, nationChild, countryBlock, currentPage);
-  let paginate = await setPagination(iterate, pageButtons,countryBlock);
-  console.log(iterate)
-  currentPage++
-  console.log(currentPage);
- 
+  let listBtns = await listButton(topCases, countryBlock, currentPage);
 }
-
+let title = document.createElement('h1');
 function getCountry(arr, container, divNums, page){
   return new Promise(function(resolve, reject){
-    // container.innerHTML = '';
-
+    container.innerHTML = '';
     let firstDiv = divNums * page;
     let lastDiv = firstDiv + divNums;
     let paginatedItems = arr.slice(firstDiv, lastDiv);
     console.log(paginatedItems)
-    // let title = document.createElement('h1');
-    // title.id = 'title-list-countries';
-    // title.innerHTML = 'STATUS OF COVID-19 CASES OF EACH COUNTRIES';
-    // nation.insertBefore(title, container);
+    title.id = 'title-list-countries';
+    title.innerHTML = 'STATUS OF COVID-19 CASES OF EACH COUNTRIES';
+    nation.insertBefore(title, container);
    
     for(i=0; i < paginatedItems.length; i++){
-      if(paginatedItems[i].All.country != null){
-     
+      if(paginatedItems[i].Country != null){
         let div = document.createElement('div');
         let img = document.createElement('img');
-        img.src = `https://www.countryflags.io/${paginatedItems[i].All.abbreviation}/flat/64.png`;
+        img.src = `https://www.countryflags.io/${paginatedItems[i].CountryCode}/flat/64.png`;
         let h1 = document.createElement('h1');
         div.className = 'top-country-with-pos';
         let countryList = document.createElement('table');
@@ -162,13 +160,13 @@ function getCountry(arr, container, divNums, page){
         let row2 = countryList.insertRow(-1);
         let row3 = countryList.insertRow(-1);
         //=========== INSIDE EACH DIV OF COUNTRY==========================================//
-        h1.append(paginatedItems[i].All.country)
+        h1.append(paginatedItems[i].Country)
         row1.insertCell(0).innerHTML = '<strong>Confirmed Cases:</strong>';
-        row1.insertCell(1).append(paginatedItems[i].All.confirmed);
+        row1.insertCell(1).append(paginatedItems[i].TotalConfirmed);
         row2.insertCell(0).innerHTML = '<strong>Deaths:</strong>';
-        row2.insertCell(1).append(paginatedItems[i].All.deaths)
-        row3.insertCell(0).innerHTML = '<strong>Population:</strong>';
-        row3.insertCell(1).append(paginatedItems[i].All.population);
+        row2.insertCell(1).append(paginatedItems[i].TotalDeaths)
+        row3.insertCell(0).innerHTML = '<strong>Recovered:</strong>';
+        row3.insertCell(1).append(paginatedItems[i].TotalRecovered);
         container.appendChild(div);
         div.appendChild(countryList);
         div.insertBefore(h1,countryList);
@@ -177,68 +175,112 @@ function getCountry(arr, container, divNums, page){
       else{
         continue;
       }
-     
+      
     }
     resolve (paginatedItems);
   })
 }
 //========================= PAGINATE COUNTRIES =======================================//
-function setPagination(arr, container, divNum){
-  return new Promise(function(){
-    container.innerHTML = '';
-    let pageCount = Math.ceil(arr.length / divNum);
-    for(i = 1; i < pageCount +1 ; i++){
+function listButton (arr, divNum, page){
+  return new Promise(function(resolve){
+    let pageNum = Math.ceil(arr.length/divNum)
+    for(i=1; i < pageNum; i++){
       let btn = document.createElement('button');
+      btn.id= i;
       btn.innerText = i;
-      btn.id = i;
-      if(btn.id === currentPage){
+      pageButtons.appendChild(btn);
+      if(btn.innerText === currentPage){
         btn.classList.add('active');
       }
-      container.append(btn);
+      btn.addEventListener('click', function(e){
+        currentPage = btn.id;
+        pageButtons.innerHTML = '';
+        displayAll();
+        console.log(currentPage);
+      })
     }
+  })
+
+}
+
+//===================== FOR THE DROPDOWN SELECT TAG =======================//
+// const optionSelector = document.querySelector('#countries');
+// let selectedOpt = false;
+// optionSelector.addEventListener('click', function(e){
+//     let option = e.target;
+//     if(option.value != '' && selectedOpt){
+//       countrySelector(option.value)
+//       return selectedOpt = false;
+//     }
+//     selectedOpt = true;
+// })
+
+//================================ MAP CREATION =================================//
+
+let heatMapData = [];
+
+// async function countrySelector(item){
+//   let response = await fetch(`https://covid-api.mmediagroup.fr/v1/cases?country=${item}`);
+//   // let response = await fetch(`country-list/csvjson.json`)
+//   let json = await response.json();
+//   return json;
+// }
+
+function iterateCountryList(item, arr){
+  return new Promise(function(){
+    for(let key in item){
+      // let opt = document.createElement('option');
+      // let createText = document.createTextNode(key);
+      // opt.appendChild(createText);
+      // opt.value = key;
+      // optionSelector.appendChild(opt);
+
+      let country = item[key];
+      for(value in country){
+        for(i=0; i < arr.length; i++){
+          if(arr[i].Country === key || arr[i].CountryCode === country.All.abbreviation){
+            let heatWeight;
+            if(arr[i].TotalConfirmed < 1,000){
+              heatWeight = 1;
+            }
+            else if(arr[i].TotalConfirmed > 1,000 && arr[i].TotalConfirmed < 5,000){
+              heatWeight = 2;
+            }
+            else if(arr[i].TotalConfirmed > 5,000 && arr[i].TotalConfirmed < 10,000){
+              heatWeight = 3;
+            }
+            else if(arr[i].TotalConfirmed > 10,000 && arr[i].TotalConfirmed < 100,000){
+              heatWeight = 4;
+            }
+            else if(arr[i].TotalConfirmed > 10000000){
+              heatWeight = 5;
+            }
+            heatMapData.push({location: new google.maps.LatLng(parseInt(country[value].lat),parseInt(country[value].long) ), weight: heatWeight})
+          }
+        }
+      }
+      
+    }
+   
   })
 }
 
-function paginationBtn(page){
-    let btn = document.createElement('button');
-    btn.innerText = page;
-    if(currentPage === page){
-      btn.classList.add('active');
-    }
-    
-    btn.addEventListener('click', function(){
-      currentPage = page;
-    })
-}
-//======================= MAP CREATION ========================//
-let map;
-// function initMap() {
-//   map = new google.maps.Map(document.getElementById("map"), {
-//     center: { lat: -34.397, lng: 150.644 },
-//     zoom: 8,
-//     mapId: '811ab3f2e9a97e5d',
-//   });
+createMap();
+function createMap(){
+  let map;
+ 
+  let ph = new google.maps.LatLng(14.599512, 120.984222);
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: ph,
+    zoom: 5,
+    mapId: '811ab3f2e9a97e5d',
+    mapTypeControl: false,
+    fullscreenControl: false,
+  });
   
-// }
-//============= TESTING HEATMAP ======================//
-
-let heatMapData = [
-  {location: new google.maps.LatLng(37.782, -122.447), weight: 2},
-  new google.maps.LatLng(37.782, -122.445),
-  {location: new google.maps.LatLng(37.782, -122.443), weight: 2},
-  new google.maps.LatLng(37.782, -122.437),
-  {location: new google.maps.LatLng(37.782, -122.435), weight: 5},
-
-];
-let ph = new google.maps.LatLng(37.774546, -122.433523);
-map = new google.maps.Map(document.getElementById('map'), {
-  center: ph,
-  zoom: 13,
-  mapId: '811ab3f2e9a97e5d'
-});
-
-var heatmap = new google.maps.visualization.HeatmapLayer({
-  data: heatMapData,
-  radius: 15
-});
-heatmap.setMap(map);
+  let heatmap = new google.maps.visualization.HeatmapLayer({
+    data: heatMapData,
+    radius: 20
+  });
+  heatmap.setMap(map);
+}
