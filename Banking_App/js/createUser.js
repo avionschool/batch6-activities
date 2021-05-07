ed220098f9400af9a0899b68d368ce3ccbd81746
Leif Bersamina <leifbersamina@gmail.com>
2021-05-07 13:39:47 +0800

    updated js/createUser.js

--- Banking_App/js/createUser.js
+++ Banking_App/js/createUser.js
@@ -2,16 +2,17 @@
 //================================ CREATE NEW USER ======================================//
 
 const newUserInput = {
-    name: document.querySelector('.register #new-name'),
+    last_name: document.querySelector('.register #new-last-name'),
+    first_name: document.querySelector('.register #new-first-name'),
     // accntNum: document.querySelector('.register #new-accnt'),
     balance: document.querySelector('.register #init-depo'),
 }
 const registerBtn = document.querySelector('.register #submit');
 registerBtn.addEventListener('click', function(e){
-    if(newUserInput.name.value.length > 1){
+    if(newUserInput.last_name.value.length > 1 && newUserInput.first_name.value.length > 1){
         e.preventDefault();
-        create_user(newUserInput.name.value,newUserInput.balance.value);
-        location.reload();
+        create_user(newUserInput.last_name.value, newUserInput.first_name.value, newUserInput.balance.value);
+        document.querySelector('.register').reset();
     }
 });
 
